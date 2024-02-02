253826f725d1299c565f9c1cc01af4ad9a89070e
SashaDemi <demidenkosasha1996@gmail.com>
2024-02-02 17:03:59 +0200

    3) feat: Adding Todos and Using Props. Replace TodoForm with TodoWrapper in App.js.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,20 +1,20 @@
 import {useState} from 'react';
 
-const TodoForm = () => {
-    // use state to store data from input.
+const TodoForm = ({addTodo}) => {
+    // using state to store data from input.
     const [value, setValue] = useState('')
 
     // This function will handle the event submission form and
     // prevents the default behavior of the onSubmit event.
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(value);
+        addTodo(value);
     }
     return (
         <form className='TodoForm' onSubmit={handleSubmit}>
             <input type='text' className='todo-input'
-            placeholder='Write your task'
-                   // Update the value state.
+                   placeholder='Write your task'
+                // Update the value state.
                    onChange={(e) => setValue(e.target.value)}/>
             <button type='submit' className='todo-button'>
                 Add Task
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,9 +1,27 @@
-import TodoForm from "./TodoForm";
+import TodoForm from './TodoForm';
+import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
+// using the UUID library to generate a unique ID number.
+const uniqueId = uuidv4();
 
 const TodoWrapper = () => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (todo) => {
+        // using spread operator to make copy
+        setTodos([...todos,
+            {
+                id: uniqueId,
+                task: todo,
+                completed: false,
+                isEditing: false
+            }]);
+
+        console.log(todos)
+    }
     return (
         <div className='TodoWrapper'>
-            <TodoForm />
+            <TodoForm addTodo={addTodo}/>
         </div>
     )
 }
